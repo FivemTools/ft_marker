2205ba24f9efbd059e6ac6766c14fff4ec288328
CanadienFr <provenchja2@msn.com>
2017-07-05 09:45:24 -0400

    Update cl_events.lua

--- cl_events.lua
+++ cl_events.lua
@@ -4,8 +4,8 @@
 -- @License: GNU General Public License v3.0
 
 RegisterNetEvent('ft_markers:ClAdd')
-AddEventHandler('ft_markers:ClAdd', function(data)
-  Add(data)
+AddEventHandler('ft_markers:ClAdd', function(name, data)
+  Add(name, data)
 end)
 
 RegisterNetEvent('ft_markers:ClRemove')
